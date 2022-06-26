8d1b99471854ed1e9b891b90a220b75fc0631ff2
Dami Jung <78487030+dami-jung@users.noreply.github.com>
2022-06-27 01:29:12 +0900

    Create simple.h

---
+++ Introduction-to-Concurrent-Programming-with-GPUs/week5/simple/simple.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <tuple>
+
+// For the CUDA runtime routines (prefixed with "cuda_")
+#include <cuda_runtime.h>
+
+__global__ void vectorMult(const float *A, const float *B, float *C, int numElements);
+float deviceMultiply(float a, float b);
+__global__ void vectorAdd(const float *A, const float *B, float *C, int numElements);
+__host__ std::tuple<float *, float *, float *> allocateHostMemory(int numElements);
+__device__ std::tuple<float *, float *, float *> allocateDeviceMemory(int numElements);
+__device__ void copyFromHostToDevice(float *h_A, float *h_B, float *d_A, float *d_B, int numElements);
+__device__ void executeKernel(float *d_A, float *d_B, float *d_C);
+__host__ void copyFromDeviceToHost(float *d_C, float *h_C, int numElements);
+__device__ void deallocateMemory(float *h_A, float *h_B, float *h_C, float *d_A, float *d_B, float *d_C);
+__device__ void cleanUpDevice();
+__device__ void performTest(float *h_A, float *h_B, float *h_C, int numElements);
